c7b10b43c23ab70c725d6d37b1daaba150543779
jcardus <asklocation.net@gmail.com>
2024-04-26 01:15:20 +0100

    start dashboard

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,39 @@
 <template>
-  <Tutorial />
+  <el-container style="height: 500px; border: 1px solid #eee">
+    <el-container>
+      <el-header style="text-align: center; font-size: 12px">
+        <span>Dashboard</span>
+        <div style="float: right;">
+          <el-dropdown>
+            <i class="el-icon-setting" style="margin-right: 15px" />
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item>View</el-dropdown-item>
+              <el-dropdown-item>Add</el-dropdown-item>
+              <el-dropdown-item>Delete</el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+          <span style="text-align: right">Tom</span>
+        </div>
+      </el-header>
+      <el-main>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
-
 <script>
 export default {
-  name: 'IndexPage'
 }
 </script>
+
+<style>
+.el-header {
+  background-color: #B3C0D1;
+  color: #333;
+  line-height: 60px;
+  text-align: center;
+}
+
+.el-aside {
+  color: #333;
+}
+</style>
