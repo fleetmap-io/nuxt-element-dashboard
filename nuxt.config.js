c04d3a6b88d1e2fa7cf95b2dc34f8061e3462901
jcardus <asklocation.net@gmail.com>
2024-04-26 08:11:48 +0100

    title

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'nuxt-element-dashboard',
+    title: 'Dashboard ' + process.env.npm_package_version,
     htmlAttrs: {
       lang: 'en'
     },
